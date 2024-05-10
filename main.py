e613526ecae194e6348a9c1b29e0da215d4f3602
szabomate100 <szabomate100@gmail.com>
2024-05-10 20:45:54 +0200

    Comments for main

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 torch.cuda.empty_cache()
 
 torch.set_default_device("cuda")
+#beállítja defalutnak a GPU
 
 images = torch.tensor([], dtype=torch.float32)
 classes = []
@@ -17,13 +18,13 @@ test_classes = []
 
 names = {"Non_Demented": 0, "Very_Mild_Demented": 1, "Mild_Demented": 2, "Moderate_Demented": 3}
 
-for folder in sorted(os.listdir("Dataset")):
+for folder in sorted(os.listdir("Dataset")):   #képek betöltése
     for idx, file in enumerate(sorted(os.listdir(f"Dataset/{folder}"))):
-        if idx > len(os.listdir(f"Dataset/{folder}")) * 0.2: continue
+        if idx > len(os.listdir(f"Dataset/{folder}")) * 0.2: continue   #memória miatti downscale
         img = Image.open(f"Dataset/{folder}/{file}").resize((128, 128)).convert("L")
         torch_image = torchvision.transforms.ToTensor()(img).to("cuda")
 
-        if idx < len(os.listdir(f"Dataset/{folder}")) * 0.15:
+        if idx < len(os.listdir(f"Dataset/{folder}")) * 0.15:   #memória miatti downscale
             images = torch.cat((images, torch_image.unsqueeze(0)), dim=0)
             temp = [0, 0, 0, 0]
             temp[names[folder]] = 1
@@ -34,7 +35,9 @@ for folder in sorted(os.listdir("Dataset")):
             temp[names[folder]] = 1
             test_classes.append(temp)
 
-y = torch.tensor(classes, dtype=torch.float32)
+
+
+y = torch.tensor(classes, dtype=torch.float32)   #a képekhez tartozó érték
 
 print(y.size())
 
@@ -45,9 +48,9 @@ class AlzheimerClassifier(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=3, padding=1),  # Grayscale images have 1 channel
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),  # Grayscale images have 1 channel #convolutional layer
+            nn.ReLU(),   #activation function
+            nn.MaxPool2d(2, 2),   #pooling layer
             nn.Conv2d(64, 32, kernel_size=3, padding=1),  # Grayscale images have 1 channel
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
@@ -58,9 +61,9 @@ class AlzheimerClassifier(torch.nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
         )
-        self.fc = nn.Sequential(
+        self.fc = nn.Sequential( ##fully connected layer
             nn.Flatten(),
-            nn.Linear(8192, 2048),  # Adjust based on output of conv layers
+            nn.Linear(8192, 2048),  #layers
             nn.ReLU(),
             nn.Linear(2048, 1024),
             nn.ReLU(),
@@ -69,35 +72,35 @@ class AlzheimerClassifier(torch.nn.Module):
             nn.Linear(512, 4)
         )
 
-    def forward(self, x):
+    def forward(self, x):#lineáris transzformáció előrébb viszi az x tengelyt
         x = self.conv(x)
-        x = self.fc(x)
+        x = self.fc(x)#GOOGLE
         return x
 
 
 model = AlzheimerClassifier()
 
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
+criterion = torch.nn.CrossEntropyLoss()     #ha több mint 2 class van akkor kell ez a loss function
+optimizer = torch.optim.Adam(model.parameters())    #legjobb optimalizer
 
-epochs = 500
-model.train()
+epochs = 500    #hány iteráció van train sorá
+model.train()   #switch to train mode
 for epoch in range(epochs):
-    outputs = model(images)
-    loss = criterion(outputs, y)
+    outputs = model(images)     #prediction
+    loss = criterion(outputs, y)    #calc loss
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    optimizer.zero_grad() #
+    loss.backward()       #backpropagation
+    optimizer.step()      #
 
     if epoch % 100 == 0:
         print(f'Epoch [{epoch}/{epochs}], Loss: {loss}')
 
 torch.save(model.state_dict(), 'model.pt')
 
-model.eval()
+model.eval()    #switch to evaluation mode
 with torch.inference_mode():
-    pred = torch.nn.functional.softmax(model(test_images), dim=1)
+    pred = torch.nn.functional.softmax(model(test_images), dim=1)   #model output + soft max => 0-1 and SUM1
     print(pred)
     accurate = 0
     for idx, p in enumerate(pred):
